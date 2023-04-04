88407cb02cb4a6e6341065624ea7bad1209be28d
Clara <clarapachecodev@gmail.com>
2023-04-04 20:10:45 -0300

    Add 'alt' attribute to Avatar component src

--- src/components/Comment.tsx
+++ src/components/Comment.tsx
@@ -34,7 +34,7 @@ export function Comment({content, onDeleteComment}:CommentProps) {
 
   return (
     <div className={styles.comment}>
-      <Avatar hasBorder={false} src='https://avatars.githubusercontent.com/u/93992748?v=4' />
+      <Avatar hasBorder={false} src='https://avatars.githubusercontent.com/u/93992748?v=4' alt='' />
 
       <div className={styles.commentBox}>
         <div className={styles.commentContent}>
